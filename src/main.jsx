b5197057ed33560a15024bd85136df491c3d4a3a
Maurii.Navarro <mauriinavarro.contacto@gmail.com>
2024-08-12 22:09:01 -0300

    modifique los estilos y la logica de la aplicacion

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Login from './components/Login'; // Importa el componente de Login
+import Login from './components/Login'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
